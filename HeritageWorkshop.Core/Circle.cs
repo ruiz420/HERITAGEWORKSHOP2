2935c36369a4fd7bf558fe35ef4d31d0df3a1c86
Your Name <roldanchakih@gmail.com>
2026-03-05 22:30:46 -0500

    primer commit

---
+++ HeritageWorkshop.Core/Circle.cs
@@ -0,0 +1,36 @@
+﻿namespace Geometry
+{
+    public class Circle: GeometricFigure
+    {
+        private double _r;
+
+        public double R
+        { 
+            get =>  _r;
+            set
+            {
+                _r = value;
+            } 
+        }
+        public Circle(string name, double r) : base(name)
+        {
+            Name = name;
+            R = r;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Math.Pow(R, 2);
+        }
+
+        public override double GetPerimeter()
+        {
+            return 2 * Math.PI * R;
+        }
+
+        private bool ValidateR(double r)
+        {
+            return r > 0;
+        }
+    }
+}
---
+++ HeritageWorkshop.Core/GeometricFigure.cs
@@ -0,0 +1,23 @@
+﻿namespace Geometry
+{
+    public abstract class GeometricFigure
+    {
+        public string Name { get; set; }
+
+        protected GeometricFigure(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+        public override string ToString()
+        {
+            return $"{Name,-15} | Area: {GetArea(),12:F5} | Perimeter: {GetPerimeter():F5}";
+        }
+
+
+    }
+}
+
